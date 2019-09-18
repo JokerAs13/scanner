6716837ddb346103c3462ed6f37e2c503e34bc9e
Samy <samy.mesbah@live.fr>
2019-09-18 16:49:30 +0200

    add scanner

---
+++ Audit_Diagnostic_Reparation.ps1
@@ -0,0 +1,20 @@
+﻿Get-CimInstance -ClassName Win32_BIOS #Infos Bios
+wmic bios get serialnumber #Juste le service TAG
+
+Get-CimInstance -ClassName Win32_Processor | Select-Object -ExcludeProperty "CIM*" #Infos processeur
+
+Get-CimInstance -ClassName Win32_ComputerSystem #Infos PC
+
+sfc /scannow #repare les fichiers système
+
+DISM /Online /Cleanup-Image /RestoreHealth #Repare images windows
+
+chkdsk.exe C: 
+#rapport d'état 'un disque dur. rejouter /F pour réparer le disque
+
+cleanmgr.exe #nettoyer lecteurs pour gain d'espace
+
+tracert google.com #Savoir a quel niveau de la route y a un pb
+
+dxdiag.exe #diagnostics pilotes et composants direct X
+
